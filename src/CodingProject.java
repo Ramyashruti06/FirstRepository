243390e8da2be7b50ae57c270469589a763dbe86
CodeTalks <info@codetalksdna.com>
2023-04-15 11:41:02 +0530

    added new line of changes

--- src/CodingProject.java
+++ src/CodingProject.java
@@ -1,5 +1,6 @@
 public class CodingProject {
-    public static void main(String[] args) {
-        System.out.println("My first coding project!");
+    public static void main(String[] args){
+    System.out.println("My first coding project in JAVA!");
+    System.out.println("Welcome to Java!");
     }
 }
